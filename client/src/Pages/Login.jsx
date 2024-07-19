d4a76656f9e00340f3baf5242b0974179fec94a6
NO ONE <sohaibikram249@gmail.com>
2024-07-19 17:25:59 +0500

    update

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -17,7 +17,7 @@ export const Login = () => {
   const { storeTokenInLS } = useAuth();
   const handleForm = async (e) => {
     e.preventDefault();
-    const response = await fetch(`https://solo-tech-server.vercel.app/api/auth/login`, {
+    const response = await fetch(`http://localhost:5000/api/auth/login`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
