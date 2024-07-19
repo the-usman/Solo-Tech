3c835f27a28eca263844bf27b1e7401301e89c27
the-usman <theusman620@gmail.com>
2024-07-19 22:40:51 +0500

    adding logs for error detection

--- server/server.js
+++ server/server.js
@@ -38,4 +38,6 @@ connect().then(() => {
   app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
+}).catch((error) => {
+  console.log("Error While adding database")
 });
