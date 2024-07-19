149a710405ddeef6d46f514ce27963d76a04f619
the-usman <theusman620@gmail.com>
2024-07-19 22:43:09 +0500

    Refactoring code

--- server/server.js
+++ server/server.js
@@ -35,11 +35,11 @@ app.get("/", (req, res) => {
 });
 
 const port = 5000;
+  
+connect().then(() => {
   app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
-connect().then(() => {
-  console.log("Connected to database");
 }).catch((error) => {
   console.log("Error While adding database")
 });
